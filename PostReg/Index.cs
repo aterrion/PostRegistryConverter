400625a409bb7505feb05ba03879ad0f5480138e
Andrey Podzorov <aterrion@gmail.com>
2017-04-06 23:25:58 +0300

    Index

--- PostReg/Index.cs
+++ PostReg/Index.cs
@@ -3,15 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace PostReg
 {
+    public class City
+    {
+        public String Name;
+        public String Region;
+    }
     public static class Index
     {
+
+        private static Dictionary<String, City> indexOfCity;
+        private static Dictionary<String, String> cityOfIndex;
+        private static Dictionary<String, String> cityOfRegion;
+
+        public static void Load(string path)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+        }
+
+
         public static bool Test(String indx)
         {
-
+            if (indexOfCity.ContainsKey(indx))
+                return true;
             return false;
+        }
+
+        public static City GetInfoFromIndex(String indx)
+        {
+            City city;
+            if (indexOfCity.TryGetValue(indx, out city))  
+                return city;
+            return null;
+        }
+        public static String GetIndexFromCity(String city)
+        {
+            string indx;
+            if (cityOfIndex.TryGetValue(city, out indx))
+                return indx;
+            return null;
+        }
+        public static String GetRegionFromCity(String city)
+        {
+            string region;
+            if (cityOfRegion.TryGetValue(city, out region))
+                return region;
+            return null;
         }
     }
 }
